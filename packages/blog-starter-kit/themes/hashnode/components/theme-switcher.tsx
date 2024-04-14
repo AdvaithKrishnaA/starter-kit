110a3b678e86058934b9204a416a1e01f65235e6
Advaith Krishna A <76763029+AdvaithKrishnaA@users.noreply.github.com>
2024-04-14 23:05:50 +0530

    Update theme-switcher.tsx

--- packages/blog-starter-kit/themes/hashnode/components/theme-switcher.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/theme-switcher.tsx
@@ -20,8 +20,6 @@ const ThemeSwitch = () => {
     return (
      <>
        <button
-          className={'button-theme-switcher'}
-          tooltipText={'Toggle theme'}
           onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
             <SunSVG className="h-[1.5rem] w-[1.3rem] dark:hidden" />
             <MoonSVG className="hidden h-5 w-5 dark:block" />
