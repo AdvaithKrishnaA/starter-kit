2e9c505e6aca6b83d9959efc83ecb15b66f17a61
Advaith Krishna A <76763029+AdvaithKrishnaA@users.noreply.github.com>
2024-04-14 23:40:20 +0530

    Update theme-switcher.tsx

--- packages/blog-starter-kit/themes/hashnode/components/theme-switcher.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/theme-switcher.tsx
@@ -5,28 +5,48 @@ import { useEffect, useState } from 'react';
 import MoonSVG from './icons/svgs/MoonSVG';
 import SunSVG from './icons/svgs/SunSVG';
 
+// const ThemeSwitch = () => {
+//     const [mounted, setMounted] = useState(false);
+//     const { theme, setTheme } = useTheme();
+
+//     useEffect(() => {
+//         setMounted(true);
+//     }, []);
+
+//     if (!mounted) {
+//         return null;
+//     }
+
+//     return (
+//      <>
+//        <button
+//           onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+//             <SunSVG className="h-[1.5rem] w-[1.3rem] dark:hidden" />
+//             <MoonSVG className="hidden h-5 w-5 dark:block" />
+//             <span className="sr-only">Toggle theme</span>
+//         </button>
+//      </>
+//     );
+// };
+
+// export default ThemeSwitch;
+
 const ThemeSwitch = () => {
-    const [mounted, setMounted] = useState(false);
-    const { theme, setTheme } = useTheme();
-
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-
-    if (!mounted) {
-        return null;
-    }
-
-    return (
-     <>
-       <button
-          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-            <SunSVG className="h-[1.5rem] w-[1.3rem] dark:hidden" />
-            <MoonSVG className="hidden h-5 w-5 dark:block" />
-            <span className="sr-only">Toggle theme</span>
-        </button>
-     </>
-    );
+const [mounted, setMounted] = useState(false);
+const { theme, setTheme } = useTheme();
+useEffect(() => {
+setMounted(true);
+}, []);
+if (!mounted) {
+return null;
+}
+const handleClick = () => {
+setTheme(theme === 'light' ? 'dark' : 'light');
 };
-
-export default ThemeSwitch;
+return (
+<CommonHeaderIconBtn handleClick={handleClick} variant="theme">
+<SunSVG className="h-[1.5rem] w-[1.3rem] dark:hidden" />
+<MoonSVG className="hidden h-5 w-5 dark:block" />
+</CommonHeaderIconBtn>
+);
+};
